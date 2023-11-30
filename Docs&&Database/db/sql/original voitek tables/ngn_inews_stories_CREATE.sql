3b6626896c989a30a653d2e2a6767e240628cd81
Alex Samih-Zade <galutziogen@gmail.com>
2023-11-30 09:32:58 +0200

    add mssql supported by voitek

---
+++ Docs&&Database/db/sql/original voitek tables/ngn_inews_stories_CREATE.sql
@@ -0,0 +1,27 @@
+USE [NewsArts]
+GO
+
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ngn_inews_stories](
+	[uid] [bigint] IDENTITY(1,1) NOT NULL,
+	[name] [nvarchar](32) NULL,
+	[lastupdate] [bigint] NOT NULL,
+	[rundown] [bigint] NOT NULL,
+	[production] [bigint] NOT NULL,
+	[ord] [int] NOT NULL,
+	[ordupdate] [bigint] NOT NULL,
+	[enabled] [bit] NOT NULL,
+	[tag] [nvarchar](max) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[uid] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
+
+GO
+
