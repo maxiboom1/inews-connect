d17f70230eeb23f8255b64dc39b5cb9cc35c9add
Alex Samih-Zade <galutziogen@gmail.com>
2024-01-01 12:41:28 +0200

    .

--- src/utilities/app-config.js
+++ src/utilities/app-config.js
@@ -1,7 +1,4 @@
 import { readFileSync } from 'fs';
-import yaml from 'js-yaml';
 
-//const appConfig =JSON.parse(readFileSync('./config.json', 'utf8'));
-const fileContents = readFileSync('./config.yaml', 'utf8');
-const appConfig = yaml.load(fileContents);
+const appConfig =JSON.parse(readFileSync('./config.json', 'utf8'));
 export default appConfig;
