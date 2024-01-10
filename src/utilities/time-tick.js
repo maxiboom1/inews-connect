ee74e642d632f7c0a676b7fae13de1ef13ddee23
Alex Samih-Zade <galutziogen@gmail.com>
2024-01-10 22:45:51 +0200

    added local shift to tick gen

--- src/utilities/time-tick.js
+++ src/utilities/time-tick.js
@@ -1,17 +1,18 @@
 function createTick(){
-    const date = new Date();
-    const epochOffset = 621355968000000000;
-    const ticksPerMillisecond = 10000;
-  
-    const ticks =
-      date.getTime() * ticksPerMillisecond + epochOffset;
-  
-    const str = ticks.toString();
-    return str;
+  const date = new Date();
+  const epochOffset = 621355968000000000; 
+  const ticksPerMillisecond = 10000;
+
+  // Get the local time zone offset in minutes, convert it to milliseconds
+  const timezoneOffsetInMilliseconds = date.getTimezoneOffset() * 60 * 1000;
+
+  // Adjust the date to local time by subtracting the timezone offset
+  const localTimeInMilliseconds = date.getTime() - timezoneOffsetInMilliseconds;
+
+  // Calculate ticks with the adjusted local time
+  const ticks = localTimeInMilliseconds * ticksPerMillisecond + epochOffset;
+
+  return ticks.toString();
 }
 
-
 export default createTick;
-
-//638381676787291640
-//638405102075590000
